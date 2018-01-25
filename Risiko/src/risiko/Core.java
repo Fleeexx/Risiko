bb66e270af7199a2d3483b0ae1ad7b4583ff5973
Felix <Felix@DESKTOP-NG54T8V>
2018-01-25 17:55:22 +0100

    Felix

--- Risiko/src/risiko/Core.java
+++ Risiko/src/risiko/Core.java
@@ -25,7 +25,7 @@ public class Core {
         dbconnection.startConnection("risiko.db");
     }
     
-    public Frage getFrage(int points, int kategorie) {
+    public void getFrage(int points, int kategorie) {
         ResultSet rs = dbconnection.executeSQLQuery("SELECT * FROM frage WHERE points = " + (points + 100) + " AND kategorie = '" + Gui.KATEGORIEN[kategorie] + "';");
         Frage f = null;
         try {
@@ -35,8 +35,13 @@ public class Core {
             System.out.println("[FEHLER] Frage konnte nicht aus der Datenbank geholt werden!");
         }
         
-        ResultSet rs = dbconnection.executeSQLQuery("SELECT * FROM antworten, fragen WHERE ")
-        return f;
+        rs = dbconnection.executeSQLQuery("SELECT * FROM antworten as a, fragen as f WHERE a.f_id = f.f_id");
+        try {
+            
+        } catch (SQLException ex) {
+            System.out.println("[FEHLER] Antworten konnten nicht aus der Datenbank geholt werden!");
+        }
+        // Gui ansteuern, Fragen und Antworten mit Setter setzen
     }
     
     
